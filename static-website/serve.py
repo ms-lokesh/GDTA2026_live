394f42eaa109116d2d1c83e5d77b628d9b4365b2
fazil-ihub <fazil.a.ihub@snsgroups.com>
2026-06-30 16:22:59 +0530

    pop up updated

--- static-website/serve.py
+++ static-website/serve.py
@@ -17,6 +17,10 @@ class ExtensionlessHTMLHandler(http.server.SimpleHTTPRequestHandler):
         return translated
 
 if __name__ == '__main__':
+    # Change working directory to the script's directory so it serves static-website files correctly
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
+    
     port = 8000
     if len(sys.argv) > 1:
         try:
